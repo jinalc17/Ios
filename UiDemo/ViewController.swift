8972f05ba10d40aa6e8d8afe413f53e26f3e76f5
Jinal <jchandibhamar1760@conestogac.on.ca>
2024-01-15 13:16:51 -0800

    Initial Commit

---
+++ UiDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UiDemo
+//
+//  Created by user239775 on 1/15/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
